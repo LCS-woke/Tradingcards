d97cb7ea5ae82424afcc7ced57c6a3ab672e9f13
Will Oke <Woke@lcs.on.ca>
2024-01-19 08:42:36 -0500

    Initial Commit

---
+++ Tradingcards/TradingcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingcardsApp.swift
+//  Tradingcards
+//
+//  Created by William Oke on 2024-01-19.
+//
+
+import SwiftUI
+
+@main
+struct TradingcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
