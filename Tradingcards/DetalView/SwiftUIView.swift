cb6eb1cabf2557cbcf59ce6acf982cde4e5d018d
Will Oke <Woke@lcs.on.ca>
2024-01-19 09:43:46 -0500

    Got the Image in and all of the nessacery stats.

--- Tradingcards/DetalView/SwiftUIView.swift
+++ Tradingcards/DetalView/SwiftUIView.swift
@@ -8,25 +8,40 @@
 import SwiftUI
 
 struct SwiftUIView: View {
+    let goalsScored: Int
+    let assists: Int
+    var imageOfPLayer: String
+    let gamesPlayed: Int
+    let playerName: String
+    
     var body: some View {
-        ZStack {
-            Rectangle()
-                .foregroundColor(.blue)
-                .frame(width: 330, height: 400)
-            
-            Rectangle()
-                .foregroundColor(.white)
-                .frame(width: 280, height: 350)
-            
-            Rectangle()
-                .foregroundColor(.yellow)
-                .frame(width: 230, height: 300)
-            
-            
+        VStack {
+            ZStack {
+                Rectangle()
+                    .foregroundColor(.blue)
+                    .frame(width: 330, height: 400)
+                
+                Rectangle()
+                    .foregroundColor(.white)
+                    .frame(width: 280, height: 350)
+                
+                Rectangle()
+                    .foregroundColor(.yellow)
+                    .frame(width: 230, height: 300)
+                
+                Image(imageOfPLayer)
+                    .resizable()
+                    .frame(width: 200, height:280)
+                
+                    
+                
+                
+            }
+            Spacer()
         }
     }
 }
 
 #Preview {
-    SwiftUIView()
+    SwiftUIView(goalsScored: 10, assists: 10, imageOfPLayer: "Jackson", gamesPlayed: 10, playerName: "Nicolas Jackson")
 }
